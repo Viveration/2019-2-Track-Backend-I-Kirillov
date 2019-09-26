17e2986c7eb6fcc6d5cbf4d23ff63fade5847ba3
Naval <viveration33@gmail.com>
2019-09-26 19:48:46 +0300

    Deleted /venv from github

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/naval/Desktop/techmain/backend/2019-2-Track-Backend-I-Kirillov/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
